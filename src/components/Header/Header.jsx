80f7ef40b1a96d78a8ee83c5b7bd1ab05fd9c2b9
shindev285 <vhcity285@gmail.com>
2024-10-28 00:21:34 +0700

    e-comic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,57 +8,12 @@ import HeaderTop from "./HeaderTop";
 import { LuShoppingCart } from "react-icons/lu";
 import { CiShop } from "react-icons/ci";
 import SearchBox from "./SearchBox/SearchBox";
+import Navigation from "./Navigations/Navigation";
+
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <div className="container">
-          <div className="row">
-            <div className="logo-wraper d-flex align-items-center col-sm-2">
-              <Link to="/">
-                <img src={logo} alt="logo" />
-              </Link>
-            </div>
-            <div className="col-sm-10 d-flex align-items-center part-2">
-              <CountryDropdown />
-
-              {/* Header Search start here */}
-              <SearchBox />
-              {/* Header Search end here */}
-              <div className="part-3 d-flex align-items-center ml-auto ">
-                <Button className="circle mr-3 ">
-                  <FaRegUser />
-                </Button>
-                <div className="cart-item ml-auto d-flex align-items-center">
-                  <span className="price">$3.35</span>
-                  <div className="position-relative ml-2">
-                  <Button className="circle ">
-                    <CiShop />
-                  </Button>
-                  <span className="count d-flex align-items-center justify-center">
-                    6
-                  </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-      <nav className="navigation">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-3 nav-part-1">
-              <Button className="all-CatTab">
-                <span className="text">All Categories</span>
-              </Button>
-            </div>
-            <div className="col-sm-9 nav-part-2">
-
-            </div>
-          </div>
-        </div>
-      </nav>
+      
     </>
   );
 };
